1c669d6dc11b02ee039cfd697a801615ed45848f
abhimanyu <joinabhimanyu@gmail.com>
2018-12-21 13:28:00 +0530

    comments removed Card component

--- src/DRS_AppUI/ClientApp/components/Card.vue
+++ src/DRS_AppUI/ClientApp/components/Card.vue
@@ -1,5 +1,4 @@
 <template>
-
   <el-card class="box-card">
     <div slot="header" class="clearfix"
          v-if="hasSlot('header')">
@@ -10,62 +9,32 @@
 
 </template>
 <script>
-/* ============
-   * Card Component
-   * ============
-   *
-   * A basic card component.
-   *
-   * Gives an idea how components work.
-   */
 
 import SlotMixin from '../mixins/slot';
 
 export default {
-  /**
-   * The name of the component.
-   */
   name: 'card',
-
-  /**
-   * The mixins that the component can use.
-   */
   mixins: [SlotMixin],
-
-  /**
-   * The properties that the component accepts.
-   */
   props: {
     contextualStyle: {
       type: String,
       required: false
     }
   },
-
-  /**
-   * The computed properties that the component can use.
-   */
   computed: {
-    /**
-     * Computed property which will compute the classes
-     * for the header of the card.
-     *
-     * @returns {Array} The classes for the header.
-     */
-    classNamesHeader() {
-      const classNames = ['card-header'];
-
-      if (this.contextualStyle) {
-        classNames.push(`bg-${this.contextualStyle}`);
-        classNames.push('text-white');
-      } else {
-        classNames.push('bg-default');
-      }
-
-      return classNames;
+	  classNamesHeader() {
+		  const classNames = ['card-header'];
+		  if (this.contextualStyle) {
+			classNames.push(`bg-${this.contextualStyle}`);
+			classNames.push('text-white');
+		  } else {
+			classNames.push('bg-default');
+		  }
+		  return classNames;
     }
   },
   mounted() {},
-  methods: {}
+		methods: {
+		}
 };
 </script>
